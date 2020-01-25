409bc36af7e8fec495556b9e0fd19387283c78c9
Carlos Andres Tobon Alzate <carlostoalz@hotmail.com>
2020-01-25 10:40:38 -0500

    modular

--- Angular Avanzado/adminpro/src/app/app-routing.module.ts
+++ Angular Avanzado/adminpro/src/app/app-routing.module.ts
@@ -1,33 +1,20 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-import { DashboardComponent } from './pages/dashboard/dashboard.component';
 import { LoginComponent } from './login/login.component';
 import { RegisterComponent } from './login/register.component';
-import { ProgressComponent } from './pages/progress/progress.component';
-import { Graficas1Component } from './pages/graficas1/graficas1.component';
 import { NopagefoundComponent } from './shared/nopagefound/nopagefound.component';
-import { PagesComponent } from './pages/pages.component';
 
 
 const routes: Routes = [
-  { 
-    path: '', 
-    component: PagesComponent,
-    children: [
-      { path: 'dashboard', component: DashboardComponent },
-      { path: 'progress', component: ProgressComponent },
-      { path: 'graficas', component: Graficas1Component },
-      { path: '', pathMatch: 'full', redirectTo: 'dashboard' }
-    ]
-  },
   { path: 'login', component: LoginComponent },
-  { path: 'register', component: RegisterComponent },
-  { path: '**', component: NopagefoundComponent },
+  { path: 'register', component: RegisterComponent }
 ];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes, { useHash: true })],
+  imports: [
+    RouterModule.forRoot(routes, { useHash: true })
+  ],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
